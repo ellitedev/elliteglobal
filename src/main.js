8e7d11a9183eec382b3cff1666ab8f8836c20956
Ellite <ellite@ellite.dev>
2022-04-14 23:22:07 +0200

    Update/Remake

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import album from 'url:./album.obj';
@@ -5,18 +6,20 @@ import floppy from 'url:./file.obj';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 50);
+const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.01, 900);
 
 var myObj;
 const objloader = new OBJLoader();
 objloader.load(album, function (object) { //load the obj file
   myObj = object;
   object.traverse(function (child) {
-    if (child instanceof THREE.Mesh) {
-      child.material.wireframe = true;
+    if (child.isMesh) {
+      var wireframeGeomtry = new THREE.WireframeGeometry(child.geometry);
+      var wireframeMaterial = new THREE.LineBasicMaterial({ color: 0x424242 });
+      var wireframe = new THREE.LineSegments(wireframeGeomtry, wireframeMaterial);
+      child.add(wireframe);
     }
-  }
-  );
+  });
   scene.add(object);
   object.position.set(0, 0, -60);
   object.scale.set(0, 0, 0);
@@ -25,19 +28,17 @@ var floppyObj;
 objloader.load(floppy, function (object) { //load the obj file
   floppyObj = object;
   object.traverse(function (child) {
-    if (child instanceof THREE.Mesh) {
-      child.material.wireframe = true;
+    if (child.isMesh) {
+      var wireframeGeomtry = new THREE.WireframeGeometry(child.geometry);
+      var wireframeMaterial = new THREE.LineBasicMaterial({ color: 0x424242 });
+      var wireframe = new THREE.LineSegments(wireframeGeomtry, wireframeMaterial);
+      child.add(wireframe);
     }
-  }
-  );
+  });
   scene.add(object);
   object.position.set(0, 0, -110);
   object.scale.set(0, 0, 0);
 });
-
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(2, 2, 5);
-scene.add(light);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
@@ -57,9 +58,9 @@ scene.add(sphere);
 function moveCamera(e) {
   const t = document.body.getBoundingClientRect().top;
   camera.position.z = t * 0.025;
-  floppyObj.scale.x = t * 0.00005;
-  floppyObj.scale.y = t * 0.00005;
-  floppyObj.scale.z = t * 0.00005;
+  floppyObj.scale.x = t * 0.00002;
+  floppyObj.scale.y = t * 0.00002;
+  floppyObj.scale.z = t * 0.00002;
   myObj.scale.x = t * 0.0025;
   myObj.scale.y = t * 0.0025;
   myObj.scale.z = t * 0.0025;
@@ -83,10 +84,11 @@ function animate() {
   requestAnimationFrame(animate);
   sphere.rotation.x += 0.005;
   sphere.rotation.y += 0.005;
-  floppyObj.rotation.x += 0.005;
-  floppyObj.rotation.y += 0.005;
-  myObj.rotation.x += 0.005;
-  myObj.rotation.y += 0.005;
+  floppyObj.rotation.x -= 0.0052;
+  floppyObj.rotation.y -= 0.0052;
+  myObj.rotation.x += 0.0055;
+  myObj.rotation.y += 0.0055;
   renderer.render(scene, camera);
 }
 animate();
+
